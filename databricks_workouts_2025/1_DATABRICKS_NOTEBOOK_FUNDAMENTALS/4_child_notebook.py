91735f2ba8cf70ac96e77d88b075c70421bd8c9d
Karthik Rajendran <167778489+karthikgh6@users.noreply.github.com>
2025-12-27 16:01:03 +0530

    Add files via upload

--- databricks_workouts_2025/1_DATABRICKS_NOTEBOOK_FUNDAMENTALS/4_child_notebook.py
+++ databricks_workouts_2025/1_DATABRICKS_NOTEBOOK_FUNDAMENTALS/4_child_notebook.py
@@ -9,10 +9,23 @@
 
 # COMMAND ----------
 
-dbutils.widgets.text("param1","default value")
-paramvalue=dbutils.widgets.get("param1")
-print("param passed from the parent nb - ",paramvalue)
+#dbutils.notebook.exit(0)
 
 # COMMAND ----------
 
-dbutils.notebook.exit(0)
+dbutils.widgets.text("table_name", "cust")
+
+# COMMAND ----------
+
+text_box_value=dbutils.widgets.get("table_name")
+print(text_box_value)
+
+# COMMAND ----------
+
+#Spark SQL
+spark.read.table(text_box_value).display()#domain specific lang(FBP)
+spark.sql(f"select * from {text_box_value}").display()#Declarative lang
+
+# COMMAND ----------
+
+dbutils.notebook.exit("notebook completed successfully")
